c2477fade59b7c7f7d808a452c1f6a23769d20dc
Developer Kabir <hakabir40@gmail.com>
2022-07-10 02:41:05 +0600

    server working , get and pass data to client

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.hkeao.mongodb.net:27017,cluster0-shard-00-01.hkeao.mongodb.net:27017,cluster0-shard-00-02.hkeao.mongodb.net:27017/?ssl=true&replicaSet=atlas-7i6kd9-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const studentCollection = client.db('tunicalabs').collection('student');
+
+        app.get('/students', async (req, res) => {
+            const query = {};
+            const cursor = studentCollection.find(query);
+            const students = await cursor.toArray();
+            res.send(students);
+        });
+
+       
+        app.post('/student', async (req, res) => {
+            const newItem = req.body;
+            const result = await studentCollection.insertOne(newItem);
+            res.send(result)
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get ('/', (req,res) =>{
+    res.send('Tunicalabs Media server running')
+});
+
+app.listen( port, ()=>{
+    console.log(`Tunicalabs server listening from port , ${port}`)
+})
